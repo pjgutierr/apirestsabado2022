04aa32d7b0b4e30772ea958754a895c05b8a0d31
pjgutierr <pjgutierrezja@cesde.net>
2022-04-02 17:32:23 -0500

    se crea version 1 api

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/api/v1/sabado/', function (req, res) {
+  res.send('HOLA SOY LA PETICION GET')
 })
 
-app.listen(3000)
+app.listen(3000, function (){
+    console.log ("SERVIDOR ENCENDIDO")
+})
